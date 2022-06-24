1545859d916df8368acd678c66f8152f84d06059
Villager <deiondrae@gmail.com>
2022-06-24 14:50:01 -0400

    Added methods for Employee object, tests for Employee object all passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,32 @@
-const Employee = require('../lib/Employee')
-
+const Employee = require('../lib/Employee');
 test('creates a new Employee object', () => {
     const employee = new Employee();
 
-    expect(employee.name).toEqual(expect.any(String))
-    expect(employee.id).toEqual(expect.any(Number))
-    expect(employee.email).toEqual(expect.any(String))
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+test('gets Employee name', () => {
+    const employee = new Employee();
+
+    expect(employee.getName()).toHaveProperty('name');
+});
+
+test('gets Employee id', () => {
+    const employee = new Employee();
+
+    expect(employee.getId()).toHaveProperty('id');
+});
+
+test('gets Employee email', () => {
+    const employee = new Employee();
+
+    expect(employee.getEmail()).toHaveProperty('email');
+});
+
+test('gets Employee title(returns Employee)', () => {
+    const employee = new Employee();
+
+    expect(employee.getRole()).toBe('Employee')
 })
