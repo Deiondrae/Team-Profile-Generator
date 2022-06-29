0193dd194defd751d657e580446e1e22cce6f081
Villager <deiondrae@gmail.com>
2022-06-29 16:08:48 -0400

    added comments to code

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -1,5 +1,6 @@
+//require file system to be able to write and copy files
 const fs = require('fs');
-
+//writes new index.html file in dist folder
 const writeFile = fileContent => {
     return new Promise((resolve, reject) => {
         fs.writeFile("./dist/index.html", fileContent, err => {
@@ -14,7 +15,7 @@ const writeFile = fileContent => {
         });
     });
 };
-
+//copies css from src folder for the index.html
 const copyFile = () => {
     return new Promise((resolve, reject) => {
         fs.copyFile("./src/style.css", "./dist/style.css", err =>{
